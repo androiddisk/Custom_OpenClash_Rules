fa080b2707642a3212a44e3956bbafb974198900
androiddisk <11062371+androiddisk@users.noreply.github.com>
2025-01-11 21:28:11 +0800

    Create update.sh

---
+++ cfg/update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+url="https://raw.githubusercontent.com/Aethersailor/Custom_OpenClash_Rules/refs/heads/main/cfg/Custom_Clash.ini"
+
+url="https://raw.githubusercontent.com/Aethersailor/Custom_OpenClash_Rules/refs/heads/main/cfg/Custom_Clash_Mainland.ini"
